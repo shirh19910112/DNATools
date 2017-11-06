136776a3e04df520b34a59f828f8c13b03bc55e9
Ronghua Shi <shirh19910112@qq.com>
2017-11-06 11:06:30 +0800

    Add files via upload

---
+++ DNATools/MyUDF.cs
@@ -0,0 +1,27 @@
+﻿using ExcelDna.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DNATools
+{
+    public class MyUDF
+    {
+        [ExcelCommand(Description = "Test Button 1", MenuName = "Excel-DNA", MenuText = "Test Button 1")]
+        public static void sutTest1()
+        {
+            MessageBox.Show("Test Button 1");
+        }
+
+        [ExcelCommand(Description = "Test Button 2", MenuName = "Excel-DNA", MenuText = "Test Button 2")]
+        public static void sutTest2()
+        {
+            MessageBox.Show("Test Button 2");
+        }
+
+        private static string string_0 = "%$#@REWQK6543JHGF432*&";
+    }
+}
---
+++ DNATools/ThisAddIn.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+using System.Reflection;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Core;
+using System.Windows.Forms;
+
+namespace DNATools
+{
+    public class ThisAddIn:IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            try
+            {
+                object xlApp = ExcelDnaUtil.Application;
+                Type xlType = xlApp.GetType();
+                CommandBars customBars = (CommandBars)xlType.InvokeMember("Commandbars", BindingFlags.GetProperty, null, xlApp, null);
+                CommandBar customBar = customBars["Excel-DNA"];
+                CommandBarButton sutBtn1 = (CommandBarButton)customBar.Controls["Test Button 1"];
+                if (sutBtn1 != null)
+                {
+                    sutBtn1.FaceId = 69;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        public void AutoClose()
+        {
+            //MessageBox.Show("Close");
+        }
+    }
+}
